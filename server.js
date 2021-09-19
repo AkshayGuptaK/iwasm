32acfa954712515b47ce5d5aa4f211bf4be58b49
Akshay Gupta <akshaygrkesh@gmail.com>
2021-09-19 23:32:02 +0530

    Safely serve static files

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
-app.use(express.static('./'));
+app.use('/', express.static('./public/'));
+app.use('/js', express.static('./js/'));
+app.use('/wasm', express.static('./build/'));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
