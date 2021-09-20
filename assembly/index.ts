cb136765f91593baf5ed4aa8dd6a81465d845bc8
Akshay Gupta <akshaygrkesh@gmail.com>
2021-09-20 19:03:13 +0530

    Fizzbuzz

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,8 +1,15 @@
 // The entry file of your WebAssembly module.
 
-declare function log(n: i32): void;
+declare function log(n: string): void;
 
 export function minusOne(n: i32): i32 {
-  log(n);
+  log(`${n}`);
   return n - 1;
+}
+
+export function fizzBuzz(n: i32): string {
+  if (n % 15 == 0) return "fizzbuzz";
+  if (n % 3 == 0) return "fizz";
+  if (n % 5 == 0) return "buzz";
+  return "";
 }
