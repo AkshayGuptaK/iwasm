f48cb24d5e6a9dc9c950f0586fc3b76894d152aa
Akshay Gupta <akshaygrkesh@gmail.com>
2021-09-20 22:45:30 +0530

    R/W Memory from/to WASM/JS

--- assembly/index.ts
+++ assembly/index.ts
@@ -13,3 +13,13 @@ export function fizzBuzz(n: i32): string {
   if (n % 5 == 0) return "buzz";
   return "";
 }
+
+export function writeMemory(): void {
+  memory.grow(2);
+  store<u8>(0, 21);
+  store<u8>(1, 99);
+}
+
+export function readMemory(ptr: i32): i32 {
+  return load<u8>(ptr);
+}
