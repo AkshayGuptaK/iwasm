2ece3545a75442b10d706f16d513e3abc465bddf
Akshay Gupta <akshaygrkesh@gmail.com>
2021-09-20 09:36:57 +0530

    Add imports

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,8 @@
 // The entry file of your WebAssembly module.
 
+declare function log(n: i32): void;
+
 export function minusOne(n: i32): i32 {
+  log(n);
   return n - 1;
 }
--- js/loader.js
+++ js/loader.js
@@ -1,20 +1,33 @@
 class WasmLoader {
-    constructor(){}
+    constructor(){
+        this._imports = {
+            "env": {
+                abort() {
+                    throw new Error('Abort called from wasm file');
+                }
+            },
+            "index": {
+                log(n) {
+                    console.log(n)
+                }
+            }
+        }
+    }
 
-    async wasm(path){
+    async wasm(path, imports = this._imports){
         console.log('Fetching ${path}');
         if (!WebAssembly.instantiateStreaming) {
-            return this.wasmFallback(path);
+            return this.wasmFallback(path, imports);
         }
-        const { instance } = await WebAssembly.instantiateStreaming(fetch(path));
+        const { instance } = await WebAssembly.instantiateStreaming(fetch(path), imports);
         return instance?.exports;
     }
 
-    async wasmFallback(path){
+    async wasmFallback(path, imports){
         console.log('Fetching ${path} using fallback');
         const response = await fetch(path);
         const bytes = await response?.arrayBuffer();
-        const { instance } = await WebAssembly.instantiate(bytes); 
+        const { instance } = await WebAssembly.instantiate(bytes, imports); 
         return instance?.exports;
     }
 }
